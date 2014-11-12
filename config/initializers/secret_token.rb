2e962c68903bf16a0d879ec0581d0750dd67a4e8
金鑫 <83966452@qq.com>
2014-11-12 14:36:40 +0800

    ManageGeneral
    ManageGeneral

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ManageGeneral::Application.config.secret_token = '3db7ed34f0c389255c4ed31d20155dbe84f7c01a839b527b274d468f23d60f32eae4e7f2457c68f9f31fcd2d4aa80951bcf0f38bc5f6565085ac258d01c4f00e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ManageGeneral::Application.config.session_store :cookie_store, key: '_Manage_General_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ManageGeneral::Application.config.session_store :active_record_store
